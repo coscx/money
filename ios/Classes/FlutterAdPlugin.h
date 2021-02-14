d50c01d8ef75e40859bda864d7a0038cc3b8035e
wan <710666260@qq.com>
2021-02-14 17:05:51 +0800

    add

--- ios/Classes/FlutterAdPlugin.h
+++ ios/Classes/FlutterAdPlugin.h
@@ -1,6 +1,4 @@
 #import <Flutter/Flutter.h>
 
 @interface FlutterAdPlugin : NSObject<FlutterPlugin>
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar flutterViewController:(FlutterViewController*) controller;
-- (instancetype)newInstance:(NSObject<FlutterPluginRegistrar>*)registrar flutterViewController:(FlutterViewController*) controller;
 @end
